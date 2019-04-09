e51fe83663ec39870a9f0a6b00644695ac5b0b05
Mamoun Saudi <mamoun@plugify.nl>
2019-04-09 17:19:40 +0200

    minor rubocop fixes

--- lib/online_betaal_platform/merchant_transaction.rb
+++ lib/online_betaal_platform/merchant_transaction.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 module OnlineBetaalPlatform
   # Merchant transaction
   class MerchantTransaction
@@ -9,35 +8,34 @@ module OnlineBetaalPlatform
       :partner_fee
 
     def initialize(attributes)
-     byebug
-     @uid             = attributes['uid']
-     @object          = attributes['object']
-     @created         = attributes['created']
-     @updated         = attributes['updated']
-     @completed       = attributes['completed']
-     @merchant_uid    = attributes['merchant_uid']
-     @checkout        = attributes['checkout']
-     @payment_method  = attributes['payment_method']
-     @payment_flow    = attributes['payment_flow']
-     @payment_details = attributes['payment_details']
-     @amount          = attributes['amount']
-     @return_url      = attributes['return_url']
-     @redirect_url    = attributes['redirect_url']
-     @notify_url      = attributes['notify_url']
-     @status          = attributes['status']
-     @metadata        = attributes['metadata']
-     @statuses        = attributes['statuses']
-     @order           = attributes['orders']
-     @escrow          = attributes['escrow']
-     @shipping_costs  = attributes['shipping_costs']
-     @partner_fee     = attributes['partner_fee']
-     @merchant_profile_uid = attributes['merchant_profile_uid']
+      @uid             = attributes['uid']
+      @object          = attributes['object']
+      @created         = attributes['created']
+      @updated         = attributes['updated']
+      @completed       = attributes['completed']
+      @merchant_uid    = attributes['merchant_uid']
+      @checkout        = attributes['checkout']
+      @payment_method  = attributes['payment_method']
+      @payment_flow    = attributes['payment_flow']
+      @payment_details = attributes['payment_details']
+      @amount          = attributes['amount']
+      @return_url      = attributes['return_url']
+      @redirect_url    = attributes['redirect_url']
+      @notify_url      = attributes['notify_url']
+      @status          = attributes['status']
+      @metadata        = attributes['metadata']
+      @statuses        = attributes['statuses']
+      @order           = attributes['orders']
+      @escrow          = attributes['escrow']
+      @shipping_costs  = attributes['shipping_costs']
+      @partner_fee     = attributes['partner_fee']
+      @merchant_profile_uid = attributes['merchant_profile_uid']
 
-     @products = attributes['products'].map do |attrs|
+      @products = attributes['products'].map do |attrs|
         OnlineBetaalPlatform::Product.new(attrs)
-     end rescue []
+      end
 
-     @total_price = @products.map{ |p| p.price.to_i }.sum || 0
+      @total_price = @products.map { |p| p.price.to_i }.sum || 0
     end
   end
 end
